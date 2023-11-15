565e2394e9826ee1219128eb66ca07893e3da1a2
Sonixray <5Q?AMc@6!X7XxyioDQ!x>
2023-11-15 22:45:55 +0100

    #53 completed

---
+++ Euroguessr/wwwroot/js/pages/account/account.js
@@ -0,0 +1,11 @@
+﻿function HideShowFunction() {
+    var x = document.getElementById("HideShowContent");
+    var y = document.getElementById("ButtonHideShowContent");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.innerHTML = "Hide";
+    } else {
+        x.style.display = "none";
+        y.innerHTML = "Show";
+    }
+}
