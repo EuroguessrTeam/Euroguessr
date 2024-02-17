722435b01f4a77d5a5d9a1c78d430e6eb2bd4285
Hugo-CASTELL <hugo.castell@outlook.fr>
2024-02-17 22:15:24 +0100

    Background SVG

--- euroguessr-front/src/App.tsx
+++ euroguessr-front/src/App.tsx
@@ -1,23 +1,11 @@
-import { SongLine } from './components/song-selection/line/SongLine'
+import { Background } from './components/background/background'
 
 function App() {
-
-  return (
-    <>
-      <div className="grid grid-cols-2 gap-2">
-        <div className="bg-white text-black font-eurotype">Eurotype</div>
-        <div className="bg-white text-black font-roboto">Roboto</div>
-        <div className="bg-yellow">yellow</div>
-        <div className="bg-yellow-light">yellow-light</div>
-        <div className="bg-purple">purple</div>
-        <div className="bg-purple-light">purple-light</div>
-        <div className="bg-pink">pink</div>
-        <div className="bg-orange">orange</div>
-        <div className="bg-blue">blue</div>
-        <div className="bg-gray">gray</div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Background className="relative h-full w-full inset-0 -mt-[10.625vh]" />
+        </>
+    );
 }
 
 export default App
