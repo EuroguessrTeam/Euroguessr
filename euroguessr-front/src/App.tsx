0fdbdca7b0e496faf86c48b68f59b8ea611fc58b
Sonixray <loangayrard@gmail.com>
2024-05-01 20:23:05 +0200

    #64: Finalizing the frontend before official release

--- euroguessr-front/src/App.tsx
+++ euroguessr-front/src/App.tsx
@@ -1,13 +1,17 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import Game from "./components/Game/Game";
-import { Background } from "./components/Background/Background";
+import About from "./components/Pages/About/About";
+import Account from "./components/Pages/Account/Account";
+import Help from "./components/Pages/Help/Help";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/play" />} /> 
       <Route path="/play" element={<Game/>} />
-      <Route path="/background" element={<Background className="relative h-full w-full inset-0 -mt-[10.625vh]"/> } />
+      <Route path="/account" element={<Account/>} />
+      <Route path="/help" element={<Help/>} />
+      <Route path="/about" element={<About/>} />
     </Routes>
   )
 }
